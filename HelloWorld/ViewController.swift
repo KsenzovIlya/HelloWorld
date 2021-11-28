03b73acd125abc796ceb7974349b4889d3dea715
Ksenzov Ilya <iksenzov1@gmail.com>
2021-11-28 22:12:49 +0300

    final version

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,23 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var helloWorldLabel: UILabel!
+    @IBOutlet var helWorldLabel: UILabel!
     @IBOutlet var toGoButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        helloWorldLabel.isHidden = true
+        helWorldLabel.isHidden = true
         toGoButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
     @IBAction func makeButtonAction() {
-        if helloWorldLabel.isHidden {
-            helloWorldLabel.isHidden = false
+        if helWorldLabel.isHidden {
+            helWorldLabel.isHidden = false
             toGoButton.setTitle("Hide Text", for: .normal)
         } else {
-            helloWorldLabel.isHidden = true
+            helWorldLabel.isHidden = true
             toGoButton.setTitle("Show Text", for: .normal)
         }
     }
